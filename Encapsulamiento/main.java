569358dcf7c35ab6b1823aefe64f9a64e9c28d05
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2023-10-20 08:30:33 -0500

    Add files via upload

---
+++ Encapsulamiento/main.java
@@ -0,0 +1,19 @@
+package Agregacion;
+public class main {
+
+    public static void main() { // Crear empleados
+        // Crear un objeto de la clase Persona
+        Persona persona = new Persona("Juan", 30);
+
+        // Acceder a los atributos a través de los métodos públicos
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Edad: " + persona.getEdad());
+
+        // Modificar la edad
+        persona.setEdad(35);
+        System.out.println("Nueva Edad: " + persona.getEdad());
+
+        // Intentar establecer una edad negativa (no se permitirá)
+        persona.setEdad(-5); // Esto mostrará un mensaje de error en la consola
+    }
+    }
