28a67f0e8e878edcd37106fb054ed85ecc924e5b
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2023-10-20 08:36:55 -0500

    Add files via upload

---
+++ association/Curso.java
@@ -0,0 +1,16 @@
+package association;
+
+
+
+// Clase Curso
+public class Curso {
+    private String nombre;
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
---
+++ association/main.java
@@ -0,0 +1,12 @@
+package Encapsulamiento;
+public class main {
+
+    public static void main() {
+         // Crear un coche
+         Coche miCoche = new Coche();
+
+         // Arrancar y apagar el coche
+         miCoche.arrancar();
+         miCoche.apagar();
+    }
+}
